7174c1cfd8e0941d371f2d04aac2e77d0999c971
Dell <Dell@DESKTOP-PNCP0EN>
2025-09-26 08:41:16 +0300

    Impelmentation Of DAL

---
+++ Comoany.G01.DAL/Data/Configurtions/DepartmentConfigurtions.cs
@@ -0,0 +1,19 @@
+﻿using Comoany.G01.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comoany.G01.DAL.Data.Configurtions
+{
+    internal class DepartmentConfigurtions : IEntityTypeConfiguration<Department>
+    {
+        public void Configure(EntityTypeBuilder<Department> builder)
+        {
+           builder.Property(D=>D.Id).UseIdentityColumn();
+        }
+    }
+}
